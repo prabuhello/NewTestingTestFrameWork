1f5ad2146ab52af0a98c2d566256da07a1eea316
prabumayupersonal <prabumayu.personal@gmail.com>
2023-10-30 15:12:10 +0530

    baseclass

--- src/test/java/com/runner/TestRunner.java
+++ src/test/java/com/runner/TestRunner.java
@@ -1,5 +1,29 @@
 package com.runner;
 
-public class TestRunner {
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import com.lib.BaseClass;
 
+public class TestRunner extends BaseClass {
+
+	@BeforeClass
+	public void setup() {
+		lauchBrowser();
+	}
+
+	@AfterClass
+	public void teardown() {
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		quitBrowser();
+	}
+	
+	@Test
+	public void  testHomege() {
+		
+	}
 }
